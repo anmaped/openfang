f360ecc76dda557fc2253aac8da9c46c64b04717
Andre Pedro <apedro.1@gmail.com>
2018-09-12 15:40:30 +0200

    rootfs update.

--- buildopenfang.sh
+++ buildopenfang.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
+CPW=$(pwd)
+mkdir _build
+
+cp fs _build/ -r
+
+cd _build
 wget https://buildroot.org/downloads/buildroot-2014.08.tar.gz
 tar xvf buildroot-2014.08.tar.gz
-cd buildroot-2014.08
-cp ../config/.config_buildroot ./.config
-cp ../config/busybox.config ./package/busybox
-cp ../config/uClibc-ng.config ./package/uclibc
+rm buildroot-2014.08.tar.gz
 
-cp ../config/.config_kernel ./
+cd buildroot-2014.08
+cp $CPW/config/.config_buildroot ./.config
+cp $CPW/config/busybox.config ./package/busybox
+cp $CPW/config/uClibc-ng.config ./package/uclibc
+
+#cp $CPW/config/.config_kernel ./
 
 make
 
@@ -22,3 +30,4 @@ make
 #  - copy hashtable_itr.patch into output/build/host-mtd-1.5.1 and patch it using the command:
 # $ patch -p0 < hashtable_itr.patch
 #
+# FOR ncurses-5.9 *USE* patch -p1 < ncurses-5.9-gcc-5.patch
