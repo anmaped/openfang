618e14da3b0eb1819d8950bed95ec5ffcc6b2005
Andre Pedro <apedro.1@gmail.com>
2018-09-20 02:15:30 +0200

    Add v4l2rtspserver tar file and some trutype fonts for osd; pppd is pre configured for using with a 4g usb dongle; Update rootfs.

--- buildroot/package/v4l2rtspserver/r4l2rtspserver.mk
+++ buildroot/package/v4l2rtspserver/r4l2rtspserver.mk
@@ -5,8 +5,8 @@
 ################################################################################
 
 V4L2RTSPSERVER_VERSION = v0.0.8
-V4L2RTSPSERVER_SITE = https://github.com/mpromonet/v4l2rtspserver.git
-V4L2RTSPSERVER_SITE_METHOD = git
+V4L2RTSPSERVER_SOURCE = v4l2rtspserver-$(V4L2RTSPSERVER_VERSION).tar.gz
+V4L2RTSPSERVER_SITE = https://raw.githubusercontent.com/anmaped/openfang/master/
 #V4L2RTSPSERVER_GIT_SUBMODULES = YES
 V4L2RTSPSERVER_DEPENDENCIES =
 V4L2RTSPSERVER_CONF_OPTS = -DLOCALE_NOT_USED
